90e9c1a5dcb8f7a8eabbfa11593c9d7762f8d28b
Aida Abdikulova <abdikulova@gmail.com>
2019-05-22 03:48:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     then return array.join
   elsif array.size == 2
     then return array.join(" and ")
-  elsif array.size == 3
+  else
     array[0,array.size-1].push("and #{array[-1]}").join(", ")
-
+end
   end
