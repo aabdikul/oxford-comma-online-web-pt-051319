96e997d6ca462e22c27fb7365814e8971a3d7399
Aida Abdikulova <abdikulova@gmail.com>
2019-05-22 03:27:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
-  array.join
-end
-
-oxford_comma(array)
+  if array.size == 1
+    then return array.join
+  elsif array.size == 2
+    then return array.join("and")
+  end
 end
